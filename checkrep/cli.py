f30504d9cae1c5b73e197b1eaf4e771cef997b92
isomoth <ic.gonzalez35@hotmail.com>
2026-09-09 11:11:27 +0200

    Logic to convert defanged input into string for API call, add TODOs for next steps

--- checkrep/cli.py
+++ checkrep/cli.py
@@ -12,15 +12,19 @@ IP_ADDRESS_BASE_URL = os.getenv('IP_ADDRESS_BASE_URL')
 
 
 @click.command()
-@click.option('--country', prompt='Enter country',
-              help='The country of the IP to analyze.')
 @click.option('--ip', prompt='Enter defanged IP address',
               help='The IP address to analyze, such as 51[.]254[.]69[.]91')
-def is_ip_malicious(country, ip):
+def is_ip_malicious(ip):
     """Basic reputation checker that takes in an IP adress and checks whether it is malicious."""
+    refanged_ip = ip.translate({ord(i): None for i in "[]"})
+    # TODO: Read up on how to interpret IP VirusTotal report
+    # Example logic:
+    # Check attributes.malicious, if count = 0, ip is benign
+    # If count = 1, ip should be investigated
+    # If count > 5, ip is malicious
     spammer = SPAMMER_IP_ADDRESS
     malicious = MALICIOUS_IP_ADDRESS
-    url = f"{IP_ADDRESS_BASE_URL}{ip}"
+    url = f"{IP_ADDRESS_BASE_URL}{refanged_ip}"
 
     headers = {
         "accept": "application/json",
@@ -32,17 +36,26 @@ def is_ip_malicious(country, ip):
     print(json.dumps(url_data, indent=2))
 
     if ip == spammer:
-        click.echo(f"IP address {ip} from {country} is a spammer!")
+        click.echo(f"IP address {ip} is a spammer!")
     elif ip == malicious:
-        click.echo(f"IP address {ip} from {country} is malicious!")
+        click.echo(f"IP address {ip} is malicious!")
     else:
-        click.echo(f"IP address {ip} from {country} is benign.")
+        click.echo(f"IP address {ip} is benign.")
 
 
 if __name__ == '__main__':
     is_ip_malicious()
 
-  # TODO: regex logic to accept defanged input and refang it upon sending the API request
-    # TODO: logic to find the key-value pair matching the IP category (spammer, phishing, etc.)
-    # TODO: logic for receiving other IoCs as input
-    # TODO: logic for uploading logs and scanning through IoCs, then returning a threat summary
+# General
+# TODO: interview SOC analysts to find out which IoCs are relevant and which data they want to see in the report
+
+# Input
+# TODO: receive and read through a text file regardless of its format
+# TODO: find all strings matching IP addresses
+# TODO: exclude private IPs
+# Output
+# TODO: exclude benign IPs from output
+# TODO: return malicious and suspicious IPs
+# TODO: logic to find the key-value pair matching the IP category (spammer, phishing, etc.)
+# TODO: logic for receiving other IoCs as input
+# TODO: logic for uploading logs and scanning through IoCs, then returning a threat summary

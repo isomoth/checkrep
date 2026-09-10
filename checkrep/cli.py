e27331d078bb0fbec13b0fd073bac4d069e9e759
isomoth <ic.gonzalez35@hotmail.com>
2026-09-10 20:22:32 +0200

    Parsing IP addresses and regex pattern

--- checkrep/cli.py
+++ checkrep/cli.py
@@ -2,6 +2,8 @@ import requests
 import json
 import click
 import os
+import ipaddress
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +11,9 @@ API_KEY = os.getenv('API_KEY')
 SPAMMER_IP_ADDRESS = os.getenv('SPAMMER_IP_ADDRESS')
 IP_ADDRESS_BASE_URL = os.getenv('IP_ADDRESS_BASE_URL')
 LOG_FILE_PATH = os.getenv('LOG_FILE_PATH')
+IP_PATTERN = re.compile(
+    r'((?:\d{1,3}\.){3}\d{1,3})'
+)
 
 
 @click.command()
@@ -33,27 +38,31 @@ def is_ip_malicious(ioc):
         print(json.dumps(url_data, indent=2))
 
     elif ioc == log_file:
-
+        valid_ip_list = []
         with open(
                 log_file, 'r', encoding="utf-8") as file:
-
             for line in file:
-                print(line.strip())
+                ip_addresses = IP_PATTERN.findall(line)
+                for ip_address in ip_addresses:
+                    ip_object = ipaddress.ip_address(ip_address)
+                    valid_ip_list.append(ip_object)
+
+            print("The log contains the following IP addresses: ", valid_ip_list)
 
     else:
         print("Invalid input, try again.")
 
 
 if __name__ == '__main__':
-    is_ip_malicious()
+    is_ip_malicious()  # noqa: E1120
 
 
 # General
 # TODO: interview SOC analysts to find out which IoCs are relevant and which data they want to see in the report
 
 # Input
+# TODO: read from user input instead of hard-coded values
 # TODO: receive and read through a text file regardless of its format
-# TODO: find all strings matching IP addresses
 # TODO: exclude private IPs
 # Output
  # TODO: Read up on how to interpret IP VirusTotal report
